4e316be95afa3b5673676134e09bacb54c69b0fc
corlard3y <corlardey@gmail.com>
2025-11-12 15:33:36 +0100

    chore: add incidents section

---
+++ src/config/KBReportList.ts
@@ -0,0 +1,22 @@
+export const KBReportList = {
+  title: 'pages.knowledge.report.explainer-section.blocks.title',
+  content: [
+    {
+      type: 'list',
+      topGap: true,
+      bottomGap: true,
+      variant: 'row',
+      items: [
+        {
+          slug: 'ecosystem-apps',
+          image: '/static/assets/website/chain/knowledge/developer-docs',
+          imageSrcSet: false,
+          title:
+            'Service Outage - Donut Testnet - 11 Nov 7 PM UTC - 12 Nov 7 AM UTC',
+          url: '/knowledge/testnet/report/incident-12-nov-2025',
+          target: '_blank',
+        },
+      ],
+    },
+  ],
+};
