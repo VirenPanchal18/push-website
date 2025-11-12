c9e01de3215f8d27c4ecb381bb2bd6a60848e8c2
harshrajat <harsh@epns.io>
2025-11-12 13:58:43 +0400

    feat: reorganize testnet page with updated content and sections

--- src/config/KBTestnetList.ts
+++ src/config/KBTestnetList.ts
@@ -12,6 +12,11 @@ export const KBTestnetList = {
         {
           type: 'text',
           valueKey:
+            'pages.knowledge.testnet.explainer-section.blocks.sections.welcome-donut',
+        },
+        {
+          type: 'text',
+          valueKey:
             'pages.knowledge.testnet.explainer-section.blocks.sections.why-push-exists',
         },
         {
@@ -22,28 +27,13 @@ export const KBTestnetList = {
         {
           type: 'text',
           valueKey:
-            'pages.knowledge.testnet.explainer-section.blocks.sections.welcome-donut',
-        },
-        {
-          type: 'text',
-          valueKey:
             'pages.knowledge.testnet.explainer-section.blocks.sections.how-to-participate',
-        },
-        {
-          type: 'text',
-          valueKey:
-            'pages.knowledge.testnet.explainer-section.blocks.sections.whats-next',
-        },
-        {
-          type: 'text',
-          valueKey:
-            'pages.knowledge.testnet.explainer-section.blocks.sections.start-building',
         },
       ],
     },
     {
       type: 'list',
-      title: 'Quick Links',
+      title: 'Developers',
       topGap: true,
       bottomGap: true,
       variant: 'row',
@@ -65,11 +55,53 @@ export const KBTestnetList = {
           target: '_blank',
         },
         {
-          slug: 'knowledge-base',
+          slug: 'push-explorer',
           image: '/static/assets/website/chain/knowledge/developer-explorer',
           imageSrcSet: false,
-          title: 'Knowledge Base',
-          url: 'http://push.org/knowledge',
+          title: 'Push Explorer',
+          url: 'https://donut.push.network',
+          target: '_blank',
+        },
+        {
+          slug: 'apply-glassroots',
+          image: '/static/assets/website/chain/knowledge/developer-explorer',
+          imageSrcSet: false,
+          title: 'Apply for Grassroots Program',
+          url: 'https://zv9atndluia.typeform.com/to/ItQ7i2sO](https://zv9atndluia.typeform.com/to/ItQ7i2sO',
+          target: '_blank',
+        },
+      ],
+    },
+    {
+      type: 'list',
+      title: 'Validators',
+      topGap: true,
+      bottomGap: true,
+      variant: 'row',
+      items: [
+        {
+          slug: 'push-validator',
+          image: '/static/assets/website/chain/knowledge/developer-explorer',
+          imageSrcSet: false,
+          title: 'Push Validator',
+          url: 'https://push.org/docs/chain/node-and-system-tools/running-push-validator/](https://push.org/docs/chain/node-and-system-tools/running-push-validator/',
+          target: '_blank',
+        },
+      ],
+    },
+    {
+      type: 'list',
+      title: 'Users',
+      topGap: true,
+      bottomGap: true,
+      variant: 'row',
+      items: [
+        {
+          slug: 'ecosystem-apps',
+          image: '/static/assets/website/chain/knowledge/developer-docs',
+          imageSrcSet: false,
+          title: 'Ecosystem Apps',
+          url: 'http://push.org/ecosystem',
           target: '_blank',
         },
         {
@@ -80,28 +112,22 @@ export const KBTestnetList = {
           url: 'http://push.org/lfpush',
           target: '_blank',
         },
+      ],
+    },
+    {
+      type: 'list',
+      title: 'Incident Reports',
+      topGap: true,
+      bottomGap: true,
+      variant: 'row',
+      items: [
         {
-          slug: 'push-explorer',
-          image: '/static/assets/website/chain/knowledge/developer-explorer',
+          slug: 'ecosystem-apps',
+          image: '/static/assets/website/chain/knowledge/developer-docs',
           imageSrcSet: false,
-          title: 'Push Explorer',
-          url: 'https://donut.push.network',
-          target: '_blank',
-        },
-        {
-          slug: 'push-validator',
-          image: '/static/assets/website/chain/knowledge/developer-explorer',
-          imageSrcSet: false,
-          title: 'Push Validator',
-          url: 'https://push.org/docs/chain/node-and-system-tools/running-push-validator/](https://push.org/docs/chain/node-and-system-tools/running-push-validator/',
-          target: '_blank',
-        },
-        {
-          slug: 'apply-glassroots',
-          image: '/static/assets/website/chain/knowledge/developer-explorer',
-          imageSrcSet: false,
-          title: 'Apply for Grassroots Program',
-          url: 'https://zv9atndluia.typeform.com/to/ItQ7i2sO](https://zv9atndluia.typeform.com/to/ItQ7i2sO',
+          title:
+            'Service Outage - Donut Testnet - 11 Nov 7 PM UTC - 12 Nov 7 AM UTC',
+          url: 'http://push.org/ecosystem',
           target: '_blank',
         },
       ],
