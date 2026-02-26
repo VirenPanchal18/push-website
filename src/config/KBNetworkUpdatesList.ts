81653f97e3e81b7ed9e894d0c0e3696f4204490a
harshrajat <harsh@epns.io>
2026-02-26 04:32:50 +0400

    chore: remove .title suffix from network updates translation key for consistency

--- src/config/KBNetworkUpdatesList.ts
+++ src/config/KBNetworkUpdatesList.ts
@@ -9,7 +9,7 @@ export const KBNetworkUpdatesList = {
       items: [
         {
           valueKey:
-            'pages.knowledge.report.explainer-section.blocks.sections.chain-upgrade-token-rebalance-25-feb-2026.title',
+            'pages.knowledge.report.explainer-section.blocks.sections.chain-upgrade-token-rebalance-25-feb-2026',
           image: '/static/assets/website/chain/knowledge/network-updates',
           imageSrcSet: false,
           url: '/knowledge/testnet/updates/chain-upgrade-token-rebalance-25-feb-2026',
